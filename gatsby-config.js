ebce823052cefc48ce48c575d2f07e372a2905ee
Ekmstudios <errol@ekmstudios.com>
2023-02-15 23:35:01 -0600

    Adding an image plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `my-gatsby-dev-js-site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 //Step 2: Define the component
 const AboutPage = () => {
     return (
         <Layout pageTitle="About Page">
             <p>Hi there. Working on my Gatsby and React skills.</p>
+            <StaticImage
+      alt="Gatsby Plugin"
+      src="../images/icon.png"
+      />
       </Layout>
     )
 }
